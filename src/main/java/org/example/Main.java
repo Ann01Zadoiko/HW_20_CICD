39977fb3368b143c6b5fd1da239061c71ae4343e
User <anna.zadoiko@gmail.com>
2023-12-15 19:30:17 +0200

    hw

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,9 +3,9 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
-        System.out.println("Hello World!");
-        System.out.println("Hello World!");
-        System.out.println("Hello World!");
-        System.out.println("Hello World!");
+    }
+
+    public int squareTriangle(int a, int b, int c){
+        return a * b * c;
     }
 }
